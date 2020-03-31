753e92ffa0a78da15400af8c83c77c349086af8b
SathwikaGone <gonesathwika@gmail.com>
2020-03-30 22:12:45 -0400

    server less

--- lambda.js
+++ lambda.js
@@ -1,6 +1,6 @@
 "use strict";
 const awsServerlessExpress = require("aws-serverless-express");
-const app = require("./server");
+const app = require("./app");
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => {
